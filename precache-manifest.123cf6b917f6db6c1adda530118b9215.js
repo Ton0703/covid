d23a298e5a3e114a1b87281f1c2c1b3d8bc81c67
Ton0703 <1120367213@qq.com>
2020-06-29 14:52:18 +0800

    Updates

---
+++ precache-manifest.123cf6b917f6db6c1adda530118b9215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cff06555bdaa4c39f9ef30d66cee93f",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "83389cda65f3fdcb5da7",
+    "url": "/covid/static/css/main.03fe8cf3.chunk.css"
+  },
+  {
+    "revision": "e0acdecb0e17d07b9c08",
+    "url": "/covid/static/js/2.7897f870.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid/static/js/2.7897f870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83389cda65f3fdcb5da7",
+    "url": "/covid/static/js/main.ef1f8cfb.chunk.js"
+  },
+  {
+    "revision": "2538616d296fec9cb619",
+    "url": "/covid/static/js/runtime-main.6f8ac736.js"
+  }
+]);
